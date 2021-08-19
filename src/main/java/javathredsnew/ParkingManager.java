22117b26c90bcc42781b72231d883b9b114b7e4a
Тимур <tvborodich@bk.ru>
2021-08-19 14:40:56 +0300

    -- done threads

---
+++ src/main/java/javathredsnew/ParkingManager.java
@@ -0,0 +1,19 @@
+package javathredsnew;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ParkingManager {
+    public static void main(String[] args) {
+//        BlockingQueue<CarPlace> blockingQueue = new ArrayBlockingQueue<>(10);
+
+//        Parking<CarPlace> cars = new Parking<>();
+//        for (int i = 1; i < 51; i++) {
+//            new Car(i, cars).start();
+//            try {
+//                Thread.sleep(new Random().nextInt(2000));
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+    }
+}
